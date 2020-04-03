b51b4d68e69de95d2ff6b1d9b4ecf6f05a3848f3
Ben Vance <benvance333@gmail.com>
2020-04-03 15:37:34 -0500

    added article model

---
+++ FantasyFootballBlog/FantasyFootballBlog/src/main/java/com/ATeam/FantasyFootballBlog/models/Article.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ATeam.FantasyFootballBlog.models;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+/**
+ *
+ * @author benvance
+ */
+
+//@Entity
+public class Article {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int article_id;
+    
+    @Column(nullable = false)   
+    private String author;
+    
+        @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + this.getArticle_id();
+        hash = 83 * hash + Objects.hashCode(this.getAuthor());
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Article other = (Article) obj;
+        if (this.getArticle_id() != other.getArticle_id()) {
+            return false;
+        }
+        if (!Objects.equals(this.author, other.author)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the article_id
+     */
+    public int getArticle_id() {
+        return article_id;
+    }
+
+    /**
+     * @param article_id the article_id to set
+     */
+    public void setArticle_id(int article_id) {
+        this.article_id = article_id;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
+
