fc8ead684dbed44b06b39371c868d684d9539298
Ben Vance <benvance333@gmail.com>
2020-04-03 10:05:14 -0500

    Bens Changes

--- sercurityDBforATeam.sql
+++ sercurityDBforATeam.sql
@@ -35,7 +35,8 @@ insert into `role`(`role`) values
 insert into `user_role`(user_id, role_id) values
 	(1,1),
     (1,2),
-    (2,2),
-    (3,2),
-    (3,1);
+    (2,1),
+    (3,1),
+    (4,1),
+    (4,2);
     
